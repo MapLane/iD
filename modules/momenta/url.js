778f417cf632b8e0006768423c68cf28ef0e140f
haoyiping <haoyiping@momenta.ai>
2018-05-04 17:08:24 +0800

    fix

--- modules/momenta/url.js
+++ modules/momenta/url.js
@@ -5,6 +5,8 @@ url.createSeg = 'createLineSeg';
 
 url.check_host='http://10.2.135.59:1123';
 
+url.showLines='http://10.2.135.59:1123/show';
+
 url.upload_package = 'http://10.2.135.59:1123/upload';
 
 url.queryFrameLocation = 'http://mapeditor.momenta.works:5123/location/keyframe';
