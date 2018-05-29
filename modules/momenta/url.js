58e21d74fd42d42f5d82aa9ccfdec64ccebc7b77
changyu <changyu@momenta.ai>
2018-05-29 20:41:23 +0800

    add fenhao

--- modules/momenta/url.js
+++ modules/momenta/url.js
@@ -10,7 +10,7 @@ url.showLines='http://172.21.2.86:1123/show';
 
 url.upload_package = 'http:/172.21.1.186:1123/upload';
 
-url.check_ok = 'http://10.2.149.54:23200/manage_task/submit_task'
+url.check_ok = 'http://10.2.149.54:23200/manage_task/submit_task';
 
 url.queryFrameLocation = 'http://mapeditor.momenta.works:5123/location/keyframe';
 
